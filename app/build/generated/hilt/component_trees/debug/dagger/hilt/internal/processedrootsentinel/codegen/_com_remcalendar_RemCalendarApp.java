02ece33b37ba0cc387042f8f8947aa521e637ec0
365days <365days@example.com>
2026-07-14 15:13:26 +0800

    Apply .gitignore and update license to BSD-3-Clause

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_remcalendar_RemCalendarApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.remcalendar.RemCalendarApp"
-)
-public final class _com_remcalendar_RemCalendarApp {
-}
